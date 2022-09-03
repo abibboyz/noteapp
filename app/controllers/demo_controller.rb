bb1a2e3c2660dc2da78ad622c2a56b7d0e439398
abinash <abi.kh03@gmail.com>
2022-09-03 22:47:56 +1000

    simple routes examples

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,21 @@
 class DemoController < ApplicationController
   layout false
   def index
+    render('hello')
+  end
+
+  def hello
+    render('index')
+  end
+
+  def call_index
+    #redirect to any controller
+    redirect_to(:controller => 'demo', :action => 'index')
+    # incase of same controller
+    # redirect_to(:action => 'index')
+  end
+
+  def youtube
+    redirect_to('http://youtube.com')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 #simple
-#  get 'demo/index'
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/call_index'
+  get 'demo/youtube'
 #default:
 #get ':controller(/:action(/:id))'
 #root route
-root 'demo#index'
+#root 'demo#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
