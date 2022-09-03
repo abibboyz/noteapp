77c0e28479605936c3f0efeaabfd4b0961fa8b68
abinash <abi.kh03@gmail.com>
2022-09-03 22:27:38 +1000

    helloworld and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+#simple
+#  get 'demo/index'
+#default:
+#get ':controller(/:action(/:id))'
+#root route
+root 'demo#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
